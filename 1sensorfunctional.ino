f2ba45f887086542b68fef37cd898e257b9020ef
Matthew-De-Guzman <65510774+Matthew-De-Guzman@users.noreply.github.com>
2022-10-19 15:54:12 -0500

    Add files via upload

--- 1sensorfunctional.ino
+++ 1sensorfunctional.ino
@@ -188,68 +188,31 @@ void dmpDataReady() {
 // ================================================================
 // ===                      INITIAL SETUP                       ===
 // ================================================================
-
-void setup() {
-    // join I2C bus (I2Cdev library doesn't do this automatically)
-    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-        Wire.begin();
-        Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
-    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-        Fastwire::setup(400, true);
-    #endif
-
-    // initialize serial communication
-    // (115200 chosen because it is required for Teapot Demo output, but it's
-    // really up to you depending on your project)
-    Serial.begin(115200);
-    while (!Serial); // wait for Leonardo enumeration, others continue immediately
-
-    // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3V or Arduino
-    // Pro Mini running at 3.3V, cannot handle this baud rate reliably due to
-    // the baud timing being too misaligned with processor ticks. You must use
-    // 38400 or slower in these cases, or use some kind of external separate
-    // crystal solution for the UART timer.
-
-    // initialize device
+void init_mpu1(){
     Serial.println(F("Initializing I2C devices..."));
     mpu.initialize();
-    mptwo.initialize();
     pinMode(INTERRUPT_PIN, INPUT);
-
-    // verify connection
+    
     Serial.println(F("Testing device connections..."));
     Serial.println(mpu.testConnection() ? F("MPU1 connection successful") : F("MPU1 connection failed"));
-    Serial.println(mptwo.testConnection() ? F("MPU2 connection successful") : F("MPU2 connection failed"));
-
-    // wait for ready
+    
     Serial.println(F("\nSend any character to begin DMP programming and demo: "));
     while (Serial.available() && Serial.read()); // empty buffer
     while (!Serial.available());                 // wait for data
     while (Serial.available() && Serial.read()); // empty buffer again
-
-    // load and configure the DMP
+    
     Serial.println(F("Initializing DMP..."));
     devStatus = mpu.dmpInitialize();
-    devStatus = mptwo.dmpInitialize();
-    // supply your own gyro offsets here, scaled for min sensitivity
-    //mpu.setXGyroOffset(89);
-    //mpu.setYGyroOffset(-15);
-    //mpu.setZGyroOffset(63);
-    //mpu.setZAccelOffset(5601); // 1688 factory default for my test chip
-
-    // make sure it worked (returns 0 if so)
     if (devStatus == 0) {
         // Calibration Time: generate offsets and calibrate our MPU6050
         mpu.CalibrateAccel(6);
-        mptwo.CalibrateAccel(6);
         mpu.CalibrateGyro(6);
-        mptwo.CalibrateGyro(6);
         mpu.PrintActiveOffsets();
-        mptwo.PrintActiveOffsets();
+        mpu.PrintActiveOffsets();
+        
         // turn on the DMP, now that it's ready
         Serial.println(F("Enabling DMP..."));
         mpu.setDMPEnabled(true);
-        mptwo.setDMPEnabled(true);
         
         // enable Arduino interrupt detection
         Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
@@ -257,6 +220,55 @@ void setup() {
         Serial.println(F(")..."));
         attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
         mpuIntStatus = mpu.getIntStatus();
+    
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
+}
+
+void init_mpu2(){
+    Serial.println(F("Initializing I2C devices..."));
+    mptwo.initialize();
+    pinMode(INTERRUPT_PIN, INPUT);
+    
+    Serial.println(F("Testing device connections..."));
+    Serial.println(mptwo.testConnection() ? F("MPU2 connection successful") : F("MPU2 connection failed"));
+    
+    Serial.println(F("\nSend any character to begin DMP programming and demo: "));
+    while (Serial.available() && Serial.read()); // empty buffer
+    while (!Serial.available());                 // wait for data
+    while (Serial.available() && Serial.read()); // empty buffer again
+    Serial.println(F("Initializing DMP..."));
+    devStatus = mptwo.dmpInitialize();
+    
+    if (devStatus == 0) {
+        // Calibration Time: generate offsets and calibrate our MPU6050
+        
+        mptwo.CalibrateAccel(6);
+        mptwo.CalibrateGyro(6);
+        mptwo.PrintActiveOffsets();
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        
+        mptwo.setDMPEnabled(true);
+        
+        // enable Arduino interrupt detection
+        Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+        Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+        Serial.println(F(")..."));
+        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
         mptwoIntStatus = mptwo.getIntStatus();
 
         // set our DMP Ready flag so the main loop() function knows it's okay to use it
@@ -264,7 +276,6 @@ void setup() {
         dmpReady = true;
 
         // get expected DMP packet size for later comparison
-        packetSize = mpu.dmpGetFIFOPacketSize();
         packetSize2 = mptwo.dmpGetFIFOPacketSize();
     } else {
         // ERROR!
@@ -275,6 +286,25 @@ void setup() {
         Serial.print(devStatus);
         Serial.println(F(")"));
     }
+
+    
+}
+void setup() {
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+
+    // initialize serial communication
+    Serial.begin(115200);
+    while (!Serial); // wait for Leonardo enumeration, others continue immediately
+
+    init_mpu1();
+    init_mpu2();
 
     // configure LED for output
     pinMode(LED_PIN, OUTPUT);
