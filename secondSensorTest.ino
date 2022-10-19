a4a711bfb82f98cdefd6a9a903fefccf3c5bf5b7
Matthew-De-Guzman <65510774+Matthew-De-Guzman@users.noreply.github.com>
2022-10-19 17:09:11 -0500

    Add files via upload

--- secondSensorTest.ino
+++ secondSensorTest.ino
@@ -1,3 +1,38 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // I2C device class (I2Cdev) demonstration Arduino sketch for MPU6050 class using DMP (MotionApps v2.0)
 // 6/21/2012 by Jeff Rowberg <jeff@rowberg.net>
 // Updates should (hopefully) always be available at https://github.com/jrowberg/i2cdevlib
@@ -58,8 +93,8 @@ THE SOFTWARE.
 // specific I2C addresses may be passed as a parameter here
 // AD0 low = 0x68 (default for SparkFun breakout and InvenSense evaluation board)
 // AD0 high = 0x69
-MPU6050 mpu(0x68);
-MPU6050 mptwo(0x69); // <-- use for AD0 high
+MPU6050 mpu(0x69);
+//MPU6050 mpu(0x69); // <-- use for AD0 high
 
 /* =========================================================================
    NOTE: In addition to connection 3.3v, GND, SDA, and SCL, this sketch
@@ -83,7 +118,7 @@ MPU6050 mptwo(0x69); // <-- use for AD0 high
 // uncomment "OUTPUT_READABLE_QUATERNION" if you want to see the actual
 // quaternion components in a [w, x, y, z] format (not best for parsing
 // on a remote host such as Processing or something though)
-#define OUTPUT_READABLE_QUATERNION
+//#define OUTPUT_READABLE_QUATERNION
 
 // uncomment "OUTPUT_READABLE_EULER" if you want to see Euler angles
 // (in degrees) calculated from the quaternions coming from the FIFO.
@@ -124,19 +159,13 @@ bool blinkState = false;
 // MPU control/status vars
 bool dmpReady = false;  // set true if DMP init was successful
 uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
-uint8_t mptwoIntStatus; 
 uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
-uint8_t devStatus2;
 uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
-uint16_t packetSize2;
 uint16_t fifoCount;     // count of all bytes currently in FIFO
 uint8_t fifoBuffer[64]; // FIFO storage buffer
-uint16_t fifoCount2; 
-uint8_t fifoBuffer2[64];
 
 // orientation/motion vars
-Quaternion queasy;           // [w, x, y, z]         quaternion container
-Quaternion queasy2;
+Quaternion q;           // [w, x, y, z]         quaternion container
 VectorInt16 aa;         // [x, y, z]            accel sensor measurements
 VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
 VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
@@ -144,33 +173,6 @@ VectorFloat gravity;    // [x, y, z]            gravity vector
 float euler[3];         // [psi, theta, phi]    Euler angle container
 float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
-// vector multiplication vars 
-VectorInt16 vec;
-VectorInt16 crossvec;
-VectorInt16 cross1;
-VectorInt16 cross2;
-VectorInt16 newvec; 
-VectorInt16 negvec;
-VectorInt16 quatvec;
-VectorInt16 devicevec;
-VectorInt16 forward0; 
-VectorInt16 forward1;
-VectorInt16 up0;
-float neg_qx; 
-float neg_qy; 
-float neg_qz; 
-float neg_qw;
-float x_cross;
-float y_cross;
-float z_cross; 
-float w_new;
-float x_new;
-float y_new;
-float z_new; 
-float vw = 0.0;
-float dotproduct;
-float angle;
-float hinge_angle;
 // packet structure for InvenSense teapot demo
 uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
 
@@ -190,39 +192,71 @@ void dmpDataReady() {
 // ================================================================
 // ===                      INITIAL SETUP                       ===
 // ================================================================
-void init_mpu1(){
+
+void setup() {
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin(0x68);
+        Wire.begin(0x69);
+        Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+    // initialize serial communication
+    // (115200 chosen because it is required for Teapot Demo output, but it's
+    // really up to you depending on your project)
+    Serial.begin(115200);
+    while (!Serial); // wait for Leonardo enumeration, others continue immediately
+
+    // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3V or Arduino
+    // Pro Mini running at 3.3V, cannot handle this baud rate reliably due to
+    // the baud timing being too misaligned with processor ticks. You must use
+    // 38400 or slower in these cases, or use some kind of external separate
+    // crystal solution for the UART timer.
+
+    // initialize device
     Serial.println(F("Initializing I2C devices..."));
     mpu.initialize();
     pinMode(INTERRUPT_PIN, INPUT);
-    
+
+    // verify connection
     Serial.println(F("Testing device connections..."));
-    Serial.println(mpu.testConnection() ? F("MPU1 connection successful") : F("MPU1 connection failed"));
-    
+    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+    // wait for ready
     Serial.println(F("\nSend any character to begin DMP programming and demo: "));
     while (Serial.available() && Serial.read()); // empty buffer
     while (!Serial.available());                 // wait for data
     while (Serial.available() && Serial.read()); // empty buffer again
-    
+
+    // load and configure the DMP
     Serial.println(F("Initializing DMP..."));
-    devStatus = mpu.dmpInitialize(0x68);
+    devStatus = mpu.dmpInitialize(0x69);
+
+    // supply your own gyro offsets here, scaled for min sensitivity
+    //mpu.setXGyroOffset(89);
+    //mpu.setYGyroOffset(-15);
+    //mpu.setZGyroOffset(63);
+    //mpu.setZAccelOffset(5601); // 1688 factory default for my test chip
+
+    // make sure it worked (returns 0 if so)
     if (devStatus == 0) {
         // Calibration Time: generate offsets and calibrate our MPU6050
         mpu.CalibrateAccel(6);
         mpu.CalibrateGyro(6);
-        mpu.PrintActiveOffsets();
-        mpu.PrintActiveOffsets();
-        
+        //mpu.PrintActiveOffsets();
         // turn on the DMP, now that it's ready
         Serial.println(F("Enabling DMP..."));
         mpu.setDMPEnabled(true);
-        
+
         // enable Arduino interrupt detection
         Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
         Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
         Serial.println(F(")..."));
         attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
         mpuIntStatus = mpu.getIntStatus();
-    
+
         // set our DMP Ready flag so the main loop() function knows it's okay to use it
         Serial.println(F("DMP ready! Waiting for first interrupt..."));
         dmpReady = true;
@@ -238,199 +272,39 @@ void init_mpu1(){
         Serial.print(devStatus);
         Serial.println(F(")"));
     }
-}
-
-void init_mpu2(){
-    while (Serial.available() && Serial.read());
-    Serial.println(F("Initializing I2C devices..."));
-    mptwo.initialize();
-    pinMode(INTERRUPT_PIN, INPUT);
-    
-    Serial.println(F("Testing device connections..."));
-    Serial.println(mptwo.testConnection() ? F("MPU2 connection successful") : F("MPU2 connection failed"));
-    
-    Serial.println(F("\nSend any character to begin DMP programming and demo: "));
-    while (Serial.available() && Serial.read()); // empty buffer
-    while (!Serial.available());                 // wait for data
-    while (Serial.available() && Serial.read()); // empty buffer again
-    Serial.println(F("Initializing DMP..."));
-    devStatus2 = mptwo.dmpInitialize(0x69);
-    
-    if (devStatus2 == 0) {
-        // Calibration Time: generate offsets and calibrate our MPU6050
-       
-        mptwo.setXGyroOffset(0);
-        mptwo.setYGyroOffset(0);
-        mptwo.setZGyroOffset(0);
-        mptwo.setXAccelOffset(0);
-        mptwo.setYAccelOffset(0);
-        mptwo.setZAccelOffset(0);
-
-        
-        //mptwo.CalibrateAccel(6);
-        //mptwo.CalibrateGyro(6);
-        //mptwo.PrintActiveOffsets();
-        // turn on the DMP, now that it's ready
-        Serial.println(F("Enabling DMP..."));
-        
-        mptwo.setDMPEnabled(true);
-        
-        // enable Arduino interrupt detection
-        Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
-        Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
-        Serial.println(F(")..."));
-        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
-        mptwoIntStatus = mptwo.getIntStatus();
-
-        // set our DMP Ready flag so the main loop() function knows it's okay to use it
-        Serial.println(F("DMP ready! Waiting for first interrupt..."));
-        dmpReady = true;
-
-        // get expected DMP packet size for later comparison
-        packetSize2 = mptwo.dmpGetFIFOPacketSize();
-    } else {
-        // ERROR!
-        // 1 = initial memory load failed
-        // 2 = DMP configuration updates failed
-        // (if it's going to break, usually the code will be 1)
-        Serial.print(F("DMP Initialization failed (code "));
-        Serial.print(devStatus);
-        Serial.println(F(")"));
-    }
-
-    
-}
-void setup() {
-    // join I2C bus (I2Cdev library doesn't do this automatically)
-    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-        Wire.begin();
-        Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
-    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-        Fastwire::setup(400, true);
-    #endif
-
-
-    // initialize serial communication
-    Serial.begin(115200);
-    while (!Serial); // wait for Leonardo enumeration, others continue immediately
-
-    init_mpu1();
-    //init_mpu2();
 
     // configure LED for output
     pinMode(LED_PIN, OUTPUT);
 }
 
 
-//Hinge joint code here
-//calculateAngle DONE
-//vectorDot DONE
-//vectorCross DONE
-//vectorNormalize
 
 // ================================================================
 // ===                    MAIN PROGRAM LOOP                     ===
 // ================================================================
 
-VectorInt16 vectorCross(Quaternion q, VectorInt16 vec)
-{
-  crossvec.x = q.y * vec.z - q.z * vec.y;
-  crossvec.y = q.z * vec.x - q.x * vec.z;
-  crossvec.z = q.x * vec.y - q.y * vec.x;
-  return crossvec;
-}
-VectorInt16 vectorCross2(VectorInt16 vec1, VectorInt16 vec2)
-{
-  crossvec.x = vec1.y * vec2.z - vec1.z * vec2.y;
-  crossvec.y = vec1.z * vec2.x - vec1.x * vec2.z;
-  crossvec.z = vec1.x * vec2.y - vec1.y * vec2.x;
-  return crossvec;
-}
-float vectorDot(Quaternion q, VectorInt16 vec)
-{
-  dotproduct = q.x * vec.x + q.y * vec.y + q.z * vec.z; 
-  return dotproduct;
-}
-float vectorDot2(VectorInt16 vec1, VectorInt16 vec2)
-{
-  dotproduct = vec1.x * vec2.x + vec1.y * vec2.y + vec1.z * vec2.z; 
-  return dotproduct;
-}
-
-VectorInt16 vectorQuatMult(Quaternion q, VectorInt16 vec)
-{
-        negvec.x = -1 * q.x;
-        negvec.y = -1 * q.y; 
-        negvec.z = -1 * q.z;
-        neg_qw = q.w;
-
-        cross1 = vectorCross(q, vec);
-        w_new = neg_qw * vw - vectorDot(q, vec);
-        newvec.x = vec.x * q.w + q.x * vw + cross1.x; 
-        newvec.y = vec.y * q.w + q.y * vw + cross1.y; 
-        newvec.z = vec.z * q.w + q.z * vw + cross1.z; 
-        
-        cross2 = vectorCross2(newvec,negvec);
-        quatvec.x = negvec.x * w_new + newvec.x * neg_qw + cross1.x;
-        quatvec.y = negvec.y * w_new + newvec.y * neg_qw + cross1.y;
-        quatvec.z = negvec.z * w_new + newvec.z * neg_qw + cross1.z;
-        
-        return quatvec;
-}
-
-VectorInt16 calculateDeviceVector(Quaternion q, VectorInt16 vec)
-{
-  devicevec = vectorQuatMult(q, vec); 
-  return devicevec;
-}
-
-float calculateAngle(VectorInt16 vec1, VectorInt16 vec2)
-{
-  dotproduct = vectorDot2(vec1,vec2);
-  angle = acos(dotproduct);
-  return angle;
-}
-
-
 void loop() {
-
-  //This is where the important part of the loop starts
     // if programming failed, don't try to do anything
     if (!dmpReady) return;
     // read a packet from FIFO
     if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) { // Get the Latest packet 
-        mpu.dmpGetQuaternion(&queasy, fifoBuffer);
-        mptwo.dmpGetQuaternion(&queasy2, fifoBuffer2);
-        
-        forward0 = calculateDeviceVector(queasy, {0.0, 0.0, 1.0});
-        forward1 = calculateDeviceVector(queasy2, {0.0, 0.0, 1.0});
-        //up0 = calculateDeviceVector(queasy, {0.0, 1.0, 0.0});
-
-        hinge_angle = calculateAngle(forward1, forward0); //add up0 if things aren't accurate
-        Serial.print("Knee Angle: "); 
-        Serial.print(hinge_angle);
-        Serial.print("\n"); 
-        
-  //everything else below is useless 
-  /*
         #ifdef OUTPUT_READABLE_QUATERNION
             // display quaternion values in easy matrix form: w x y z
-            mpu.dmpGetQuaternion(&queasy, fifoBuffer);
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
             Serial.print("quat\t");
-            Serial.print(queasy.w);
+            Serial.print(q.w);
             Serial.print("\t");
-            Serial.print(queasy.x);
+            Serial.print(q.x);
             Serial.print("\t");
-            Serial.print(queasy.y);
+            Serial.print(q.y);
             Serial.print("\t");
-            Serial.println(queasy.z);
+            Serial.println(q.z);
         #endif
 
-        
         #ifdef OUTPUT_READABLE_EULER
             // display Euler angles in degrees
-            mpu.dmpGetQuaternion(&queasy, fifoBuffer);
-            mpu.dmpGetEuler(euler, &queasy);
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetEuler(euler, &q);
             Serial.print("euler\t");
             Serial.print(euler[1] * 180/M_PI);
             Serial.print("\t");
@@ -439,9 +313,9 @@ void loop() {
 
         #ifdef OUTPUT_READABLE_YAWPITCHROLL
             // display Euler angles in degrees
-            mpu.dmpGetQuaternion(&queasy, fifoBuffer);
-            mpu.dmpGetGravity(&gravity, &queasy);
-            mpu.dmpGetYawPitchRoll(ypr, &queasy, &gravity);
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
             Serial.print("ypr\t");
             Serial.print(ypr[1] * 180/M_PI);
             Serial.print("\t");
@@ -450,9 +324,9 @@ void loop() {
 
         #ifdef OUTPUT_READABLE_REALACCEL
             // display real acceleration, adjusted to remove gravity
-            mpu.dmpGetQuaternion(&queasy, fifoBuffer);
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
             mpu.dmpGetAccel(&aa, fifoBuffer);
-            mpu.dmpGetGravity(&gravity, &queasy);
+            mpu.dmpGetGravity(&gravity, &q);
             mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
             Serial.print("areal\t");
             Serial.print(aaReal.x);
@@ -465,11 +339,11 @@ void loop() {
         #ifdef OUTPUT_READABLE_WORLDACCEL
             // display initial world-frame acceleration, adjusted to remove gravity
             // and rotated based on known orientation from quaternion
-            mpu.dmpGetQuaternion(&queasy, fifoBuffer);
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
             mpu.dmpGetAccel(&aa, fifoBuffer);
-            mpu.dmpGetGravity(&gravity, &queasy);
+            mpu.dmpGetGravity(&gravity, &q);
             mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-            mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &queasy);
+            mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
             Serial.print("aworld\t");
             Serial.print(aaWorld.x);
             Serial.print("\t");
@@ -477,11 +351,8 @@ void loop() {
             Serial.print("\t");
             Serial.println(aaWorld.z);
         #endif
-
-        //sorry bro I'll make this look less ugly... 
-        
-        
-    */    
+    
+  
         // blink LED to indicate activity
         blinkState = !blinkState;
         digitalWrite(LED_PIN, blinkState);
